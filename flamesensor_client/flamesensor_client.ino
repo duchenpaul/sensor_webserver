98706d0932015ec58fbe1ea5211668c99c6301f6
duchenpaul <duchenpaul@gmail.com>
2019-07-15 23:54:47 +0800

    Add dynamic wifi config

--- flamesensor_client/flamesensor_client.ino
+++ flamesensor_client/flamesensor_client.ino
@@ -1,25 +1,72 @@
-#include <ESP8266WiFi.h>
+#include <ESP8266WiFi.h>          //ESP8266 Core WiFi Library (you most likely already have this in your sketch)
+
+#include <DNSServer.h>            //Local DNS Server used for redirecting all requests to the configuration portal
+#include <ESP8266WebServer.h>     //Local WebServer used to serve the configuration portal
+#include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager WiFi Configuration Magic
+
 #include <ESP8266HTTPClient.h>
 #include <ArduinoJson.h>
 
-const char* ssid = "ssid";
-const char* password = "password";
+
+
+
+#define HOSTNAME "ESP8266-OTA-"
 
 int heart_beat_ind = 10;
 
+void configModeCallback (WiFiManager *myWiFiManager);
+
+void configModeCallback (WiFiManager *myWiFiManager) {
+  Serial.println("Entered config mode");
+  Serial.println(WiFi.softAPIP());
+  //if you used auto generated SSID, print it
+  Serial.println(myWiFiManager->getConfigPortalSSID());
+
+}
+
 void setup()
 {
+    Serial.begin(115200);
+    //WiFiManager
+    //Local intialization. Once its business is done, there is no need to keep it around
+    WiFiManager wifiManager;
+    // Uncomment for testing wifi manager
+    wifiManager.resetSettings();
+    wifiManager.setAPCallback(configModeCallback);
+
+    //or use this for auto generated name ESP + ChipID
+    wifiManager.autoConnect();
+
+    //Manual Wifi
+    //WiFi.begin(WIFI_SSID, WIFI_PWD);
+    String hostname(HOSTNAME);
+    hostname += String(ESP.getChipId(), HEX);
+    WiFi.hostname(hostname);
+
+
+    // Connecting to WiFi network
+    Serial.println();
+    Serial.print("Connecting to ");
+
+    // WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+    }
+    Serial.println("");
+    Serial.println("WiFi connected");
+
+
+    // Printing the ESP IP address
+    Serial.println(WiFi.localIP());
+
     pinMode(LED_BUILTIN, OUTPUT);
     digitalWrite(LED_BUILTIN, LOW);
     delay(500);
     digitalWrite(LED_BUILTIN, HIGH);
-    Serial.begin(115200);
-    WiFi.begin(ssid, password);
+    // WiFi.begin(ssid, password);
 
-    while (WiFi.status() != WL_CONNECTED) {
-        delay(1000);
-        Serial.println("Connecting...");
-    }
 }
 
 void loop()
